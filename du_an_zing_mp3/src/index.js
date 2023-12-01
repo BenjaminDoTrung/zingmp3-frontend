52e15fb385c9855496acedac0f4dc0105dd9d14c
Nguyen-Trong-Phu-19-09-1999 <nguyentrongphu529@gmail.com>
2023-12-01 17:18:23 +0700

    fe

--- du_an_zing_mp3/src/index.js
+++ du_an_zing_mp3/src/index.js
@@ -1,18 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
-import {Provider} from "react-redux";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import reduxConfig from "./redux";
+import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const {store,persistor} = reduxConfig();
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <Provider>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </Provider>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
