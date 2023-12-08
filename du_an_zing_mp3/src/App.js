f5dc6ee7a383ab77056a79a14da8fa0f8291697c
Nguyen-Trong-Phu-19-09-1999 <nguyentrongphu529@gmail.com>
2023-12-08 09:15:13 +0700

    demo

--- du_an_zing_mp3/src/App.js
+++ du_an_zing_mp3/src/App.js
@@ -30,18 +30,7 @@ function App() {
                   </Route>
               </Routes>
           </div>
-          <ToastContainer
-              position={"bottom-right"}
-              autoClose={5000}
-              hideProgressBar={false}
-              newestOnTop={false}
-              closeOnClick
-              rtl={false}
-              pauseOnFocusLoss
-              draggable
-              pauseOnHover
-              theme={"light"}
-          />
+
       </>
 
   );
