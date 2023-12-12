c8ee8a5601aabeb8e2d5523b88a544c83f782532
Trung Do CodeGym <trung96ecvn@gmail.com>
2023-12-12 15:44:07 +0700

    mmmmm

--- du_an_zing_mp3/src/page/public/UpdateUser.js
+++ du_an_zing_mp3/src/page/public/UpdateUser.js
@@ -2,29 +2,66 @@ import Header from "../../components/Header";
 import {Field, Form, Formik} from "formik";
 import axios from "axios";
 import {useEffect, useState} from "react";
-
+import {storage} from "../../FireBase/FireBaseConfig";
+import {
+    ref,
+    uploadBytes,
+    getDownloadURL
+} from "firebase/storage";
 
 export default function UpdateUser() {
     const id = localStorage.getItem("idUser")
     const [user, setUser] = useState({})
+    const [uploadedImageUrl, setUploadedImageUrl] = useState(undefined);
+    const [image, setImage] = useState(null);
+
     useEffect(() => {
         axios.get('http://localhost:8080/users/' + id).then((res) => {
             console.log(res.data)
             setUser(res.data)
         })
     }, [])
+    const uploadFile = (image, id_user) => {
+        if (image === null) return
+        const imageRef = ref(storage, `IMG-ZingMP3/${image.name}`);
+        uploadBytes(imageRef, image).then((snapshot) => {
+            getDownloadURL(snapshot.ref).then((url) => {
+                setUploadedImageUrl(url); // Lưu URL sau khi upload thành công vào state mới
+                console.log("image uploaded successfully", url);
+                console.log("image uploaded successfully", uploadedImageUrl);
+                user.url_img = url;
+                axios.put("http://localhost:8080/users/" + id, user).then((res)=>{
+                    console.log("huong")
+                    alert("huong")
+                })
+            });
+        });
+    };
+
     return (
           <>
             <div className={'h-[70px] px-[59px] flex items-center'}>
                 <Header/>
             </div>
                 <Formik
-                    initialValues={user}
-                    onSubmit={(user1) => {
-                        updateProfile(user1)
-
+                    initialValues={{
+                        userName: user.userName,
+                        email: user.email,
+                        phone: user.phone,
+                        url_img: user.url_img,
+                        dayOfBirth: user.dayOfBirth
                     }}
-                    enableReinitialize={true}>
+                    enableReinitialize={true}
+                    onSubmit={(user1) => {
+                        axios.put("http://localhost:8080/users/"+ id, user1).then((res)=>{
+                            localStorage.setItem("user", res.data.username)
+                            console.log("pheo")
+                            alert("Cập nhật thành công")
+                        }).catch(()=>{
+                            alert("false")
+                        })
+                    }}
+                    >
                     <Form>
                         <div className="p-3 my-5 d-flex flex-column w-50">
                             <div>Name</div>
@@ -45,30 +82,14 @@ export default function UpdateUser() {
                             </div>
                             <div>IMG</div>
                             <div className="form-outline mb-4">
-                                <input id='form2' type='file'/>
+                                <input id='form2' name="url_img" type='file' onChange={(event)=>{
+                                    uploadFile(event.target.files[0], id)
+                                }}/>
                             </div>
-                            <button className="mb-4" type={"submit"}>Update</button>
+                            <button className="mb-4" type="submit" >Update</button>
                         </div>
                     </Form>
                 </Formik>
-
         </>
     )
-
-    function updateProfile(user1) {
-        let userAcc = {
-            id : user.id ,
-            name : user1.name ,
-            email : user1.email ,
-            phone : user1.phone ,
-            dayOfBirth : user1.dayOfBirth ,
-        }
-        axios.put('http://localhost:8080/users/' + userAcc.id ,userAcc).then((res) => {
-            console.log("user", res.data.username)
-            localStorage.setItem("user", res.data.username)
-            alert("Cập nhật thành công")
-        }).catch(() => {
-            alert("cekkon")
-        })    }
-
 }
