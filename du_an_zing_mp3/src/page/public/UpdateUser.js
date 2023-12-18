bda7b6a747a6c213df54e1b6b5b5066bb443b491
Nguyễn Bá Tuấn Anh <anhnbt.it@gmail.com>
2023-12-18 22:42:57 +0700

    refactor update user

--- du_an_zing_mp3/src/page/public/UpdateUser.js
+++ du_an_zing_mp3/src/page/public/UpdateUser.js
@@ -9,13 +9,14 @@ import {
     getDownloadURL
 } from "firebase/storage";
 import React from 'react';
-
+import { Button, notification, Space } from 'antd';
 
 export default function UpdateUser() {
     const id = localStorage.getItem("idUser")
     const [user, setUser] = useState({})
     const [uploadedImageUrl, setUploadedImageUrl] = useState(undefined);
     const [image, setImage] = useState(null);
+    const [api, contextHolder] = notification.useNotification();
 
     useEffect(() => {
         axios.get('http://localhost:8080/users/' + id).then((res) => {
@@ -40,6 +41,14 @@ export default function UpdateUser() {
         });
     };
 
+    const openNotificationWithIcon = (title, desc) => {
+        console.log(title, desc);
+        api.success({
+            message: title,
+            description: desc,
+            placement: 'top'
+        });
+    };
 
     return (
         <>
@@ -55,7 +64,6 @@ export default function UpdateUser() {
                 onSubmit={(user1) => {
                     axios.put("http://localhost:8080/users/"+ id, user1).then((res)=>{
                         localStorage.setItem("user", res.data.username)
-                        console.log("pheo")
                         alert("Cập nhật thành công")
                     }).catch(()=>{
                         alert("false")
@@ -67,46 +75,45 @@ export default function UpdateUser() {
                             <div className="container py-5 h-100" >
                                 <div className="row d-flex justify-content-center align-items-center h-100">
                                     <div className="col col-xl-10">
-                                        <div className="card" style={{borderRadius: 1}}>
-                                            <div className="row g-0">
-                                                <div className="col-md-6 col-lg-5 d-none d-md-block" style={{height:200}}>
+                                        <div className="card">
+                                            <div className="row no-gutters">
+                                                <div className="col-md-4">
                                                     {user.url_img == null ? <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp"
                                                                                  alt="login form" className="img-fluid" /> :
                                                         <img src= {user.url_img}
                                                              alt="login form" className="img-fluid" style={{width:400, height: 350, marginTop: 100,paddingLeft: 50}} />}
                                                 </div>
-                                                <div className="col-md-6 col-lg-7 d-flex align-items-center">
-                                                    <div className="card-body p-4 p-lg-5 text-black">
-                                                        <div className="d-flex align-items-center mb-3 pb-1" style={{height: 50}}>
-                                                            <span className="h1 fw-bold mb-0">Update User</span>
+                                                <div className="col-md-8">
+                                                    <div className="card-body">
+                                                        <div className="d-flex align-items-center mb-3 pb-1">
+                                                            <span className="h1 fw-bold mb-0">Cập nhật thông tin cá nhân</span>
                                                         </div>
 
-                                                        <div className="form-outline mb-4" style={{height: 50}}>
-                                                            <Field name="email" type="email" id="form2Example17" class="form-control form-control-lg" />
-                                                            <label className="form-label" htmlFor="form2Example17">Email address</label>
+                                                        <div className="form-group mb-2">
+                                                            <label className="" htmlFor="form2Example17">Địa chỉ email</label>
+                                                            <Field name="email" type="email" id="form2Example17" placeholder="Nhập địa chỉ email" class="form-control form-control-lg" />
                                                         </div>
 
-                                                        <div className="form-outline mb-4" style={{height: 50}}>
-                                                            <Field name = "userName" type="text" id="form2Example27" className="form-control form-control-lg" />
-                                                            <label className="form-label" htmlFor="form2Example27">Username</label>
+                                                        <div className="form-group mb-2">
+                                                            <label className="" htmlFor="form2Example27">Tài khoản</label>
+                                                            <Field name="userName" type="text" id="form2Example27" placeholder="Nhập tài khoản" className="form-control form-control-lg" />
                                                         </div>
-                                                        <div className="form-outline mb-4" style={{height: 50}}>
-                                                            <Field name = "phone" type="text" id="form2Example27" className="form-control form-control-lg" />
-                                                            <label className="form-label" htmlFor="form2Example27">Phone</label>
+                                                        <div className="form-group mb-2">
+                                                            <label className="" htmlFor="form2Example27">Số điện thoại</label>
+                                                            <Field name="phone" type="text" id="form2Example27" placeholder="Nhập số điện thoại" className="form-control form-control-lg" />
                                                         </div>
-                                                        <div className="form-outline mb-4" style={{height: 50}}>
-                                                            <Field name = "dayOfBirth" type="text" id="form2Example27" className="form-control form-control-lg" />
-                                                            <label className="form-label" htmlFor="form2Example27">Day Off Birth</label>
+                                                        <div className="form-group mb-2">
+                                                            <label className="" htmlFor="form2Example27">Ngày sinh (dd/YY/yyyy)</label>
+                                                            <Field name="dayOfBirth" type="text" id="form2Example27" placeholder="Định dạng dd/YY/yyyy" className="form-control form-control-lg" />
                                                         </div>
-                                                        <div className="form-outline mb-4" style={{height: 50}}>
-                                                            <input name = "img" type="file" id="form2Example27" className="form-control form-control-lg" onChange={(event) => {
+                                                        <div className="form-group mb-2">
+                                                            <label className="" htmlFor="form2Example27">Ảnh đại diện</label>
+                                                            <input name="img" type="file" id="form2Example27" className="form-control form-control-lg" onChange={(event) => {
                                                                 uploadFile(event.target.files[0], id)
                                                             }}/>
-                                                            <label className="form-label" htmlFor="form2Example27">Img</label>
                                                         </div>
-
-                                                        <div className="pt-1 mb-4" style={{height: 50}}>
-                                                            <button className="btn btn-dark btn-lg btn-block" style={{color: "violet"}}>Chỉnh sửa</button>
+                                                        <div className="pt-1 mb-4">
+                                                            <button className="btn btn-primary btn-lg btn-block">Chỉnh sửa</button>
                                                         </div>
                                                     </div>
                                                 </div>
