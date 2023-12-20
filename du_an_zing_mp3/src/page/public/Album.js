3321fb5d9176b1e7bbfdf45a7939c87af2702731
trananh195 <trananhtnu195@gmail.com>
2023-12-20 10:55:41 +0700

    demo rightSidebar

--- du_an_zing_mp3/src/page/public/Album.js
+++ du_an_zing_mp3/src/page/public/Album.js
@@ -7,6 +7,7 @@ import {useDispatch} from "react-redux";
 import * as actions from '../../store/actions'
 import {SongItem} from "../../components";
 import axios from "axios";
+import album from "../../accsets/album.jpg"
 
 const Album = () => {
     const [songs, setSongs] = useState([])
@@ -25,16 +26,16 @@ const Album = () => {
     return (
         <div className={'flex gap-8 w-full px-[59px]'}>
             <div className={'flex-none w-1/4 border border-red-500 flex flex-col gap-1'}>
-                <img src={playlistData?.thumbnailM} alt='' className={'w-full object-contain rounded-md shadow-md '}/>
+                <img src={album} alt='' className={'w-full object-contain rounded-md shadow-md '}/>
 
                 <div className={'flex flex-col items-center gap-1'}>
-                    <h3 className={'text-[20px] font-bold text-gray-800'}>{playlistData?.title}Title</h3>
+                    <h3 className={'text-[20px] font-bold text-gray-800'}>Album Hot</h3>
                     <span className={'flex gap-2 items-center text-gray-500 text-xs'}>
                     <span>Update</span>
                         {/*<span>(moment.unix(playlistData?.contentLastUpdate).format("DD/MM/YYYY"</span>*/}
                 </span>
-                    <span className={'flex gap-2 items-center text-gray-500 text-xs'}>{playlistData?.artistName}artist</span>
-                    <span className={'flex gap-2 items-center text-gray-500 text-xs '}>{playlistData?.like}like</span>
+                    <span className={'flex gap-2 items-center text-gray-500 text-xs'}>Nhiều nghệ sĩ</span>
+
                 </div>
             </div>
             <div className={'flex-auto border border-green-500'}>
